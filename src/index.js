24d34a3751adaf6a0f470b584c8d1f89f34086ac
Adam El Amrani <adelamco1091@outlook.com>
2022-02-18 18:29:26 +0100

    Created async function to start server @ index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+require("dotenv");
+const chalk = require("chalk");
+const debug = require("debug");
 
+const port = process.env.SERVER_PORT;
+const server = require("./server")(async () => {
+  try {
+    await server(port);
+  } catch (error) {
+    debug(chalk.red(error.message));
+  }
+});
