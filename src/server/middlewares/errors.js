81d7550634a6ab2a36ce21daf156ebeea4017159
adam-elamrani <adelamco1091@outlook.com>
2022-02-19 22:33:02 +0100

    Added exception line for generalError, as it will be used later

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -5,6 +5,7 @@ const notFound = (req, res) => {
   res.status(404).json({error: true, message: "Endpoint not found"});
 };
 
+// eslint-disable-next-line no-unused-vars
 const generalError = (err, req, res, next) => {
   debug(chalk.red(`Error: ${err.message}`));
   const errorCode = err.code ?? 500;
