60d6f8d303357cac030f70fa769b0eedbb4c9d3e
adam-elamrani <adelamco1091@outlook.com>
2022-02-20 00:00:27 +0100

    Created id robot test

--- src/server/controllers/robotsControllers.test.js
+++ src/server/controllers/robotsControllers.test.js
@@ -1,5 +1,5 @@
 const Robot = require("../../db/models/Robot");
-const getRobots = require("./robotsControllers");
+const {getRobots, getIdRobot} = require("./robotsControllers");
 
 jest.mock("../../db/models/Robot");
 
@@ -24,6 +24,32 @@ describe("Given a getRobots controller", () => {
       await getRobots(null, res);
       expect(Robot.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenLastCalledWith( robots );
+    });
+  });
+});
+
+describe("Given a getIdRobot controller", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call the json method of the robot in the response", async () => {
+      const robot = {
+        id: 1,
+        name: "G-MOD",
+        resistance: 2,
+      };
+
+      const res = {
+        json: jest.fn(),
+      };
+
+      const req = {
+        params: { id: 1 },
+      };
+
+      Robot.findById = jest.fn().mockResolvedValue(robot);
+
+      await getIdRobot(req, res);
+
+      expect(res.json).toHaveBeenCalledWith(robot);
     })
   })
 })
