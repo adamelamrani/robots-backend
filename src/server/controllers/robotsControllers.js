975ed9e23b1df08a3f160e7e13a2e143d4d70043
adam-elamrani <adelamco1091@outlook.com>
2022-02-20 10:51:19 +0100

    Created delete and post method

--- src/server/controllers/robotsControllers.js
+++ src/server/controllers/robotsControllers.js
@@ -21,9 +21,59 @@ const getIdRobot = async (req, res, next) => {
       next(error)
     }
   } catch (error) {
-    error.code = 400;
-    next(error);
+    debug(chalk.red(`Error: ${error.message}`));
+    next(error)
   };
 }
 
-module.exports = { getRobots, getIdRobot }
+const deleteRobot = async (req, res, next) => {
+  const {id} = req.params;
+  try {
+    const robot = await Robot.findByIdAndDelete(id);
+    if (robot) {
+      res.json(robot);
+    }else {
+      const error = new Error("Robot not found. Beep-Boop");
+      error.code = 404;
+      next(error)
+    }
+  } catch (error) {
+    debug(chalk.red(`Error: ${error.message}`));
+    next(error)
+  }
+}
+
+const updateRobot = async (req, res, next) => {
+  const {id} = req.body;
+  try {
+    const updatedRobot = await Robot.findByIdAndUpdate(id);
+    if (updatedRobot) {
+      res.json(updatedRobot);
+
+    } else {
+      const error = new Error("Robot not found. Beep-Boop");
+      error.code = 404;
+      next(error)
+    }
+  } catch (error) {
+    debug(chalk.red(`Error: ${error.message}`));
+    next(error)
+  }
+}
+
+const postRobot = async (req, res, next) => {
+  const newRobot = req.body;
+  try {
+    const addNewRobot = await Robot.create(newRobot);
+    res.status(201);
+    res.json(addNewRobot);
+    debug(chalk.bgBlackBright(`Created new request ${addNewRobot}`));
+  } catch (error) {
+    debug(chalk.red(`Error: ${error.message}`));
+    next(error)
+  }
+}
+
+
+
+module.exports = { getRobots, getIdRobot, deleteRobot, updateRobot, postRobot}
